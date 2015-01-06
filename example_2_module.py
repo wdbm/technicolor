2d6de67034d2023ab351ab5705bf490e1f5ff040
Will Breaden Madden <zicodian@gmail.com>
2015-01-06 14:55:40 +0000

    add module logging example

---
+++ example_2_module.py
@@ -0,0 +1,8 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+def function1():
+    print("printout of function 1")
+    log.info("example INFO message in module")
+    log.debug("example DEBUG message in module")
