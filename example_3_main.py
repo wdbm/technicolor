ab86c7a29aae68e3ba6d1fe70667fd2167fef8e3
William Dmitri Breaden Madden <william.dmitri.breaden.madden@cern.ch>
2015-12-17 16:55:05 +0100

    add propyte logger

---
+++ example_3_main.py
@@ -0,0 +1,25 @@
+import logging
+import technicolor
+import example_2_module
+
+def main():
+
+    verbose = True
+
+    global log
+    log = logging.getLogger("propyte")
+    logging.root.addHandler(technicolor.ColorisingStreamHandler())
+
+    # logging level
+    if verbose:
+        logging.root.setLevel(logging.DEBUG)
+    else:
+        logging.root.setLevel(logging.INFO)
+
+    log.info("example INFO message in main")
+    log.debug("example DEBUG message in main")
+    
+    example_2_module.function1()
+ 
+if __name__ == '__main__':
+    main()
---
+++ example_3_module.py
@@ -0,0 +1,9 @@
+import logging
+
+log = logging.getLogger("propyte")
+
+def function1():
+
+    print("printout of function 1")
+    log.info("example INFO message in module")
+    log.debug("example DEBUG message in module")
